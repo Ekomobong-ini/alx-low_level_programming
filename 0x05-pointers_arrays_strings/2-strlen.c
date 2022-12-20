9cd8b845c2290cbeac1a747abcd379ea0474dad9
Ekomobong-ini <Ekomobongini20@gmail.com>
2022-12-20 15:45:48 -0800

    This report, by its very length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - A function that returns the length of a string
+ * @s: first integer
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (*s++)
+		count++;
+
+	return (count);
+}
