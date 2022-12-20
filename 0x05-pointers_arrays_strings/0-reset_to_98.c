9153350106ada1b3c705c5beb0e358cd779f509c
Ekomobong-ini <Ekomobongini20@gmail.com>
2022-12-20 15:10:43 -0800

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
- * reset_to_98 - a function that takes a pointer to an int as parametr
- * and update th value it points to to 98
- * @n: input
- * Return: n
+ * reset_to_98 - A function that takes a pointer to an int as a parameter
+ * @n: First integer
+ *
+ * Return: Always 0 (success)
  */
 void reset_to_98(int *n)
 {
