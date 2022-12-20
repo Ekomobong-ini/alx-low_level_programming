2a599ab8347f09ac9bcda1b11a2ac3e5b5d70841
Ekomobong-ini <Ekomobongini20@gmail.com>
2022-12-20 05:11:45 -0800

    dont swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swap the values of two intergsr
+ * @a: integer to swap
+ * @b: integer to sawp
+ */
+void swap_int(int *a, int *b)
+{
+	int a;
+
+	n = *a;
+	*a = *b;
+	*b = n;
+}
