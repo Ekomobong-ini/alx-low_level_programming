214b1a09e77799750f9d119704e707e836feeebe
Ekomobong-ini <Ekomobongini20@gmail.com>
2022-12-21 15:01:17 -0800

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts - A function that prints the character of
+ * every other string
+ * @str: integer
+ *
+ * Return: 0
+ */
+void puts2(char *str);
+{
+	int index = 0, len = 0;
+
+	while (str[index++])
+		len++;
+
+	for (index = 0; index < len; index += 2)
+		_putchar(str[index]);
+
+	_putchar('\n');
+}
