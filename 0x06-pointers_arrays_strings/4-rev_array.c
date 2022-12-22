118e454f7fd512dcf227c72863b9127fa6b544f2
Ekomobong-ini <Ekomobongini20@gmail.com>
2022-12-22 10:37:03 -0800

    I am a kind of paranoid in reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * reverse_array - a function that reverse arrays
+ * @a: string
+ * @n: integer
+ *
+ * Return: 0;
+ */
+void reverse_array(int *a, int n)
+{
+	int tmp, index;
+
+	for (index = 0; index < n; index++)
+	{
+		n--;
+		tmp = a[index];
+		a[index] = a[n];
+		a[n] = tmp;
+	}
+
+}
