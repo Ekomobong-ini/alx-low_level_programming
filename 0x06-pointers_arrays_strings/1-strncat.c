73f86b81523e5285223b2ad00788ddc49057997e
Ekomobong-ini <Ekomobongini20@gmail.com>
2022-12-22 03:50:48 -0800

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,30 @@
+#include "maim.h"
+
+/**
+ * _strncat - joins two strings n bytes
+ * @dest: copy to
+ * @src: copy from
+ * @n: number of char to be copied
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+
+	int s;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	s = 0;
+	while (s < n && src[s] != '\0')
+	{
+		dest[i] = src[s];
+		i++;
+		s++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
